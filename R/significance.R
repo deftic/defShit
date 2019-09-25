8d332b8fe15e7c7e0da94d51f884b8ae3980717e
Christoph Bachmann <advert@gmx.net>
2019-09-25 15:43:20 +0200

    Update significance.R

--- R/significance.R
+++ R/significance.R
@@ -8,7 +8,7 @@ significance <- function(df) {
   data.frame(
     target <- deparse(substitute(df)), # extract name of dataframe
     pVal <- (t.test(df$value ~ df$genotype, paired = FALSE))$p.value,
-    pText <- ifelse(pVal >= 0.05, pText <- "ns",
+    pText <- ifelse(pVal >= 0.05, pText <- "",
                     ifelse(pVal < 0.05 & pVal >= 0.01, pText <- "*",
                            ifelse(pVal < 0.01 & pVal >= 0.001, pText <- "**",
                                   ifelse(pVal < 0.001 & pVal >= 0.0001, pText <- "***",
